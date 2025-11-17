e93dfc7aaf3eb31f73b73ab3a397295031c3fd7c
Lakito0100 <lukasluethi0100@gmail.com>
2025-11-17 09:04:21 +0100

    working

--- Framework_V3/models/Frostmodell_V1.py
+++ Framework_V3/models/Frostmodell_V1.py
@@ -180,8 +180,8 @@ class Frostmodell_Edge:
 
             # Unterrelaxation aus Stabilitätsgründen
 
-            omega_T = 0.5
-            omega_w = 0.5
+            omega_T = 1.0
+            omega_w = 1.0
 
             T_f_old[:] = (1 - omega_T) * T_f_old + omega_T * T_f_new
             w_f_old[:] = (1 - omega_w) * w_f_old + omega_w * w_f_new
@@ -202,7 +202,8 @@ class Frostmodell_Edge:
                 w_sat_i = self.w_sat_coolprop(st.T_e[i, j], cfg.p_a)
                 source = cfg.C * st.rho_a[i, j] * (st.w_e[i, j] - w_sat_i)
                 d_rho = source * cfg.dt
-                st.rho_e[i, j] = np.clip(st.rho_e[i, j] + d_rho, 1.0, cfg.rho_i)
+                #st.rho_e[i, j] = np.clip(st.rho_e[i, j] + d_rho, 1.0, cfg.rho_i)
+                st.rho_e[i, j] = 250.0
 
         # s_e-Update
         for j in range(gs.ntheta):
